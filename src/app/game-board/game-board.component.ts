35f5088c471b77bfc59f8fe450dab809b6288626
Aleksa <aleksad09@gmail.com>
2022-10-27 14:18:22 +0200

    Implement game logic

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { resolve } from 'dns';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-game-board',
@@ -8,7 +10,7 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 export class GameBoardComponent implements OnInit {
   @ViewChild('board', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
 
-  private ctx: CanvasRenderingContext2D;
+  ctx: CanvasRenderingContext2D;
 
   constructor() { }
 
@@ -18,26 +20,30 @@ export class GameBoardComponent implements OnInit {
 
     this.ctx = this.canvas.nativeElement.getContext('2d');
 
-    this.board = new Array(this.rows);
-    for (let i = 0; i < this.board.length; i++) {
-      this.board[i] = new Array(this.cols);
-      for (let j = 0; j < this.board[i].length; j++) {
-        if (Math.random() <= 0.99) this.board[i][j] = 0;
-        else this.board[i][j] = 1;
-      }
-    }
-    console.log(this.board);
-    this.render();
+    this.createBoard();
   }
 
   canvasWidth: number = 400;
   canvasHeight: number = 400;
-  resolution: number = 20;
+  resolution: number = 10;
 
   rows: number = this.canvasHeight / this.resolution;
   cols: number = this.canvasWidth / this.resolution;
 
   board: number[][];
+
+  async createBoard() {
+    this.board = new Array(this.rows);
+    for (let i = 0; i < this.board.length; i++) {
+      this.board[i] = new Array(this.cols);
+      for (let j = 0; j < this.board[i].length; j++) {
+        if (Math.random() <= 0.9) this.board[i][j] = 0;
+        else this.board[i][j] = 1;
+      }
+    }
+    this.render();
+    this.play();
+  }
 
   render() {
     for (let i = 0; i < this.board.length; i++) {
@@ -50,6 +56,67 @@ export class GameBoardComponent implements OnInit {
         this.ctx.stroke();
       }
     }
+  }
+
+  nextGeneration() {
+    let future: number[][] = new Array(this.rows);
+    for (let i = 0; i < this.board.length; i++) {
+      future[i] = new Array(this.cols);
+      for (let j = 0; j < this.board[i].length; j++) {
+        let aliveNeighbours: number = this.countNeighbours(i, j);
+        if (this.board[i][j] == 1 && (aliveNeighbours < 2 || aliveNeighbours > 3)) {
+          future[i][j] = 0; // Cell dies due to underpopulation or overpopulation
+        }
+        else if (this.board[i][j] == 0 && aliveNeighbours == 3) {
+          future[i][j] = 1; // Cell becomes alive due to reproduction
+        }
+        else {
+          future[i][j] = this.board[i][j]; // Cell remains the same
+        } 
+      }
+    }
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board[0].length; j++) {
+        this.board[i][j] = future[i][j];
+      }
+    }
+  }
+
+  countNeighbours(row: number, col: number): number {
+    let aliveNeighbours: number = 0;
+    for (let i = row - 1; i <= row + 1; i++) {
+      for (let j = col - 1; j <= col + 1; j++) {
+        if (i < 0 || i >= this.board.length || j < 0 || j >= this.board[i].length || i == row && j == col)
+          continue;
+        if (this.board[i][j] == 1) 
+          aliveNeighbours++;
+      }
+    }
+    return aliveNeighbours;
+  }
+
+  async play() {
+    while (this.stopped == false) {
+      await new Promise(f => setTimeout(f, 1000));
+      this.nextGeneration();
+      this.render();
+    }
+  }
+
+  stopped: boolean = true;
+
+  start() {
+    this.stopped = false;
+    this.play();
+  }
+
+  stop() {
+    this.stopped = true;
+  }
+
+  next() {
+    this.nextGeneration();
+    this.render();
   }
 
 }
